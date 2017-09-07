c1e62b7e89bed12840e3ed0303f2a5bbe648810f
ketchupyan <hugyanyi@gmail.com>
2017-09-07 16:15:18 +0800

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,65 @@
+<template>
+  <div id="app">
+    <el-button :loading="true" @click="doDefaultTap" size="large">默认按钮</el-button>
+    <el-button icon="edit" type="primary" :disabled="true">主要按钮</el-button>
+    <el-button type="text">文字按钮</el-button>
+    <div>
+      <el-button-group>
+        <el-button type="primary" icon="arrow-left">上一页</el-button>
+        <el-button type="primary">下一页<i class="el-icon-arrow-right el-icon--right"></i></el-button>
+      </el-button-group>
+      <el-button-group>
+      <el-button type="primary" icon="edit"></el-button>
+      <el-button type="primary" icon="share"></el-button>
+      <el-button type="primary" icon="delete"></el-button>
+      </el-button-group>
+    </div>
+    <div>
+      <el-button type="text" @click="dialogVisible=true">点击打开Dialog</el-button>
+      <el-dialog :visible.sync="dialogVisible" title="提示一下">
+        <span>这是一段信息</span>
+        <span slot="footer" class="dialog-footer">
+            <el-button @click="dialogVisible=false">取消</el-button>
+            <el-button type="primary" @click="dialogVisible=false">确定</el-button>
+        </span>
+      </el-dialog>
+    </div>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import ElDialog from './components/ElDialog.vue'
+import ElButton from './components/ElButton'
+import ElButtonGroup from './components/ElButtonGroup'
+export default {
+  name: 'app',
+  data() {
+    return {
+      dialogVisible: false
+    }
+  },
+  methods: {
+    doDefaultTap(evt) {
+      console.log('tapped')
+      console.log(evt)
+    }
+  },
+  components: {
+    'el-button': ElButton,
+    'el-button-group': ElButtonGroup,
+    'el-dialog': ElDialog
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/ElButton.vue
@@ -0,0 +1,49 @@
+<template>
+  <button
+  :disabled="disabled" 
+  @click="handleClick"
+  class="el-button" 
+  :class="[type?'el-button--'+type:'',
+  size?'el-button--'+size:'',
+  {
+    'is-disabled':disabled,
+    'is-loading' : loading,
+    'is-plain' : plain
+  }]">
+    <i v-if="loading" class="el-icon-loading"></i>
+    <i @click="handleInnerClicks" :class="'el-icon-'+icon" v-if="icon&&!loading"></i>
+    <span v-if="$slots.default"></span>  <slot></slot>
+  </button>
+</template>
+<script>
+export default {
+    props: {
+      type: {
+        type: String,
+        default: 'default'
+      },
+      size: String,
+      icon: {
+        type: String,
+        default: ''
+      },
+      disabled: Boolean,
+      loading: Boolean,
+      plain: Boolean,
+      autofocus: Boolean,
+    },
+    methods: {
+      handleClick (evt) {
+      this.$emit('click', evt)
+    },
+    handleInnerClicks(evt) {
+      this.$emit('click', evt)
+    },
+    }
+}
+</script>
+
+<style>
+
+</style>
+
